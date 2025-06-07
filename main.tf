67ab62c49cf62aa323fc29dda1deaf92e9417c9b
John Grinwis <john@grinwis.com>
2025-06-07 21:10:13 +0200

    moved required_providers to versions.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  # using import so make sure we're using at least TF 1.5.0
-  required_version = ">= 1.5.0"
-  required_providers {
-    akamai = {
-      source  = "akamai/akamai"
-      version = ">= 8.0.0"
-    }
-  }
-}
-
 provider "akamai" {
   edgerc         = "~/.edgerc"
   config_section = "gss-demo"
